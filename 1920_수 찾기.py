0b37c9fa90917f9048d6b478f745a22353f3ad13
yongjunism <yjcho.dev@gmail.com>
2022-08-09 23:17:43 +0900

    수 찾기(실버4) - 자료구조(set), 이분탐색

--- 1920_수 찾기.py
+++ 1920_수 찾기.py
@@ -1,13 +1,38 @@
+# 이분 탐색 풀이
+import sys
+input = sys.stdin.readline
+
+def binary_search(arr, target):
+    low, high = 0, n-1
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            return 1
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return 0
+
+n = int(input())
+arr1 = sorted(list(map(int, input().split())))
+m = int(input())
+arr2 = list(map(int, input().split()))
+
+for i in arr2:
+    print(binary_search(arr1, i))
+
+# set() 풀이
 import sys
 input = sys.stdin.readline
 
 n = int(input())
-arr1 = list(map(int, input().split()))
+arr1 = set(list(map(int, input().split())))
 m = int(input())
 arr2 = list(map(int, input().split()))
 
-for element in arr2:
-    if element in arr1:
+for i in arr2:
+    if i in arr1:
         print(1)
     else:
         print(0)
