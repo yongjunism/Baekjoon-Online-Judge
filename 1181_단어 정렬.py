8a48b6d2b4fa2edb120507fe7ca1217cc6f1cc95
yongjunism <yjcho.dev@gmail.com>
2022-08-09 23:24:02 +0900

    단어 정렬(실버5) - 정렬

--- 1181_단어 정렬.py
+++ 1181_단어 정렬.py
@@ -2,8 +2,9 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-words = [input().rstrip() for _ in range(n)]
-words = list(set(words))
+word = [input().rstrip() for _ in range(n)]
 
-words.sort(key=lambda x: (len(x), x))
+word = list(set(word))
+word.sort(key=lambda x: (len(x), x))
+
 print('\n'.join(word))
