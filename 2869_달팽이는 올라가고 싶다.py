485cb81c6e7dde668f6f21101f71567afb58d13c
yongjunism <yjcho.dev@gmail.com>
2022-08-13 00:34:00 +0900

    달팽이는 올라가고 싶다(실버5) - 수학

--- 2869_달팽이는 올라가고 싶다.py
+++ 2869_달팽이는 올라가고 싶다.py
@@ -1,6 +1,13 @@
+# 조건문 분기 
 a, b, v = map(int, input().split())
 
 if (v-a) % (a-b) == 0:
     print((v-a)//(a-b)+1)
 else:
     print((v-a)//(a-b)+2)
+
+# math.ceil() 이용
+import math
+
+a, b, v = map(int, input().split())
+print(math.ceil((v-b)/(a-b)))
