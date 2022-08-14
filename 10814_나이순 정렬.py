3ceb5ba33f2df060c01cb7b7b41d513cc8c4e4b4
yongjunism <yjcho.dev@gmail.com>
2022-08-14 23:13:07 +0900

    나이순 정렬(실버5) - 정렬

--- 10814_나이순 정렬.py
+++ 10814_나이순 정렬.py
@@ -3,6 +3,7 @@ input = sys.stdin.readline
 
 n = int(input())
 data = [list(input().rstrip().split()) for _ in range(n)]
+
 data.sort(key = lambda x: int(x[0]))
 for age, name in data:
     print(age, name)
