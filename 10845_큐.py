9eb255a97dda9eceba511c5c997726cfd44874c9
yongjunism <yjcho.dev@gmail.com>
2022-08-29 01:28:34 +0900

    큐(실버4) - 자료구조(list), 큐

--- 10845_큐.py
+++ 10845_큐.py
@@ -1,7 +1,32 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 
 n = int(input())
+q = []
+
 for _ in range(n):
-    cmd = input
+    cmd = list(map(str, input().rstrip().split()))
+    if cmd[0] == 'push':
+        q.append(cmd[1])
+    elif cmd[0] == 'pop':
+        if q:
+            print(q.pop(0))
+        else:
+            print(-1)
+    elif cmd[0] == 'size':
+        print(len(q))
+    elif cmd[0] == 'empty':
+        if q:
+            print(0)
+        else:
+            print(1)
+    elif cmd[0] == 'front':
+        if q:
+            print(q[0])
+        else:
+            print(-1)
+    elif cmd[0] == 'back':
+        if q:
+            print(q[-1])
+        else:
+            print(-1)
