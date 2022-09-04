d373d1dc09dfb201fc5977fee8a5bf87fe96d62b
yongjunism <yjcho.dev@gmail.com>
2022-09-04 14:36:07 +0900

    소수 구하기(실버3) - 에라토스테네스의 체

---
+++ 1929_소수 구하기.py
@@ -0,0 +1,12 @@
+m, n = map(int, input().split())
+
+a = [False, False] + [True] * (n-1)
+primes = []
+
+for i in range(2, n+1):
+    if a[i]:
+        primes.append(i)
+        for j in range(2*i, n+1, i):
+            a[j] = False
+res = [i for i in primes if i >= m]
+print('\n'.join(map(str, res)))
