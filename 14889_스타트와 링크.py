708ae0a25391454a74d8f86ee3d2eacb650ec1e1
Yongjun Cho <97726655+yongjunism@users.noreply.github.com>
2022-09-20 21:45:56 +0900

    스타트와 링크(실버2) - 브루트포스, 백트래킹

--- 14889_스타트와 링크.py
+++ 14889_스타트와 링크.py
@@ -1,26 +1,27 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-stat = [list(map(int, input().split())) for _ in range(n)]
-team_start = []
-diff = []
-
-def dfs(num, last, n):
-    global team_start, diff
-    if num == n // 2:
+def dfs(cnt, last, n):
+    global team, diff
+    if cnt == n // 2:
         start, link = 0, 0
         for i in range(n):
             for j in range(n):
-                if i in team_start and j in team_start:
+                if i in team and j in team:
                     start += stat[i][j]
-                elif i not in team_start and j not in team_start:
+                elif i not in team and j not in team:
                     link += stat[i][j]
         diff.append(abs(start - link))
     else:
         for i in range(last, n):
-            team_start.append(i)
-            dfs(num+1, i, n)
-            team_start.remove(i)
+            if i not in team:
+                team.append(i)
+                dfs(cnt+1, i, n)
+                team.remove(i)
+                
+n = int(input())
+stat = [list(map(int, input().split())) for _ in range(n)]
+team, diff = [], []
+
 dfs(0, 0, n)
 print(min(diff))
